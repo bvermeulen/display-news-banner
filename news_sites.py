5c635d229b8b5ca7d4f09e2ede12e7dcc51074ea
Bruno Vermeulen <bruno_vermeulen2001@yahoo.com>
2018-07-31 14:25:20 +0700

    changine place with pack and other improvements

---
+++ news_sites.py
@@ -0,0 +1,23 @@
+'''  Module containing the news site with rss '''
+
+news_list = {'CNN World News':
+               'http://rss.cnn.com/rss/edition_world.rss',
+               'The Guardian':
+               'https://www.theguardian.com/business/economics/rss',
+               'Nu.nl':
+               'http://www.nu.nl/rss/Algemeen',
+               'BBC World News':
+               'http://feeds.bbci.co.uk/news/world/rss.xml',
+               'BBC Technology':
+               'http://feeds.bbci.co.uk/news/technology/rss.xml',
+               'BBC Business':
+               'http://feeds.bbci.co.uk/news/business/rss.xml',
+               'Hacker fp':
+               'https://hnrss.org/frontpage',
+               'Django':
+               'https://hnrss.org/newest?q=Django',
+               'Thailand':
+               'https://tradingeconomics.com/thailand/rss',
+               'Mad Money':
+               'https://www.cnbc.com/id/15838459/device/rss/rss.html',
+               }
